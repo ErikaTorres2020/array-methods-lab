489e86646f790f8a090275ded1e21fb7863c6404
Erika Torres <+>
2021-02-26 10:34:29 -0500

    commit tree 2328bca20fb8dbdd950541faccdd02335c006dc6

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  arrary = ["array"]
+  array.sort
 end
 
 def using_reverse(array)
-
+  array == ["arrays"]
+  array.reverse
 end
 
 def using_first(array)
-
+  array == ["element"]
+  array.first
 end
 
 def using_last(array)
-
+  array == ["element"]
+  array.last
 end
 
 def using_size(array)
+  array == ["array"]
+  array.size
 
 end
